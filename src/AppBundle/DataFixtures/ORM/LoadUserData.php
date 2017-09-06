ce2467f55224dc0c4f7f26ae8b0a20c573237289
Corentin Regnier <corentin.regnier59@gmail.com>
2017-09-06 16:32:44 +0200

    jenkins link

--- src/AppBundle/DataFixtures/ORM/LoadUserData.php
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -1,7 +1,9 @@
 <?php
 
 /*
- * This file is part of the TestVagrant Project.
+ * This file is part of the ValepImmo Project.
+ *
+ * (c) Corentin Régnier <corentin.regnier59@gmail.fr>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
